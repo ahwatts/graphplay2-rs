2bcbb4066549f1a0255a3a10a54202c6f001b6a5
Andrew Watts <ahwatts@gmail.com>
2016-06-26 01:20:46 -0400

    Put the integrator together for the body.

--- src/body.rs
+++ src/body.rs
@@ -1,29 +1,101 @@
 use nalgebra::*;
+use integrator::*;
+use std::cell::RefCell;
+use std::ops::{Add, AddAssign, Mul, MulAssign};
+use std::rc::{Rc, Weak};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Debug)]
 pub struct Body {
     pub mass: f32,
     pub position: Vector3<f32>,
     pub velocity: Vector3<f32>,
     force: Vector3<f32>,
+    integrator: Option<Box<Integrator<Phase, f32>>>,
 }
 
 impl Body {
-    pub fn new() -> Body {
-        Default::default()
-    }
-
-    pub fn update(&mut self, dt: f32) {
-    }
-}
-
-impl Default for Body {
-    fn default() -> Body {
-        Body {
+    pub fn new() -> Rc<RefCell<Body>> {
+        let body = Rc::new(RefCell::new(Body {
             position: Vector3::new(0.0, 0.0, 0.0),
             velocity: Vector3::new(0.0, 0.0, 0.0),
             force: Vector3::new(0.0, 0.0, 0.0),
             mass: 0.0,
+            integrator: None,
+        }));
+
+        let equation = Box::new(BodyStateEquation {
+            body: Rc::downgrade(&body),
+        });
+
+        let init_phase = Phase {
+            position: Vector3::new(0.0, 0.0, 0.0),
+            momentum: Vector3::new(0.0, 0.0, 0.0),
+        };
+
+        let init_time = 0.0;
+
+        body.borrow_mut().integrator = Some(Box::new(Euler::new(equation, init_phase, init_time)));
+
+        body
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        self.integrator.as_mut().unwrap().step(&dt);
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Phase {
+    position: Vector3<f32>,
+    momentum: Vector3<f32>,
+}
+
+impl Dependent<f32> for Phase {}
+
+impl AddAssign for Phase {
+    fn add_assign(&mut self, other: Phase) {
+        self.position += other.position;
+        self.momentum += other.momentum;
+    }
+}
+
+impl MulAssign<f32> for Phase {
+    fn mul_assign(&mut self, other: f32) {
+        self.position *= other;
+        self.momentum *= other;
+    }
+}
+
+impl Add<Phase> for Phase {
+    type Output = Phase;
+
+    fn add(mut self, other: Phase) -> Phase {
+        self += other;
+        self
+    }
+}
+
+impl Mul<f32> for Phase {
+    type Output = Phase;
+
+    fn mul(mut self, other: f32) -> Phase {
+        self *= other;
+        self
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BodyStateEquation {
+    body: Weak<RefCell<Body>>,
+}
+
+impl FirstOrderODE<Phase, f32> for BodyStateEquation {
+    fn derivative(&self, _pos: &Phase, _time: &f32) -> Phase {
+        let strong_body = self.body.upgrade().unwrap();
+        let body = strong_body.borrow();
+        Phase {
+            position: body.velocity,
+            momentum: body.force / body.mass,
         }
     }
 }
--- src/integrator.rs
+++ src/integrator.rs
@@ -3,26 +3,33 @@ use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Mul, MulAssign};
 
 pub trait Dependent<F: Float> where
-    Self: Sized + Clone + Copy + Debug,
+    Self: Sized + Debug + Clone, // + Clone + Copy + Debug,
     Self: AddAssign + MulAssign<F>,
     Self: Add + Mul<F, Output = Self>
 {}
 
 pub trait Independent where
-    Self: Sized + Clone + Copy + Debug + Float,
+    Self: Float + Debug, // + Sized + Clone + Copy + Debug + Float,
     Self: AddAssign + Add
 {}
 
-pub trait FirstOrderODE<X: Dependent<T>, T: Independent> {
+impl<F: Float + Debug + AddAssign> Independent for F {}
+
+pub trait FirstOrderODE<X: Dependent<T>, T: Independent>
+    where Self: Debug
+{
     fn derivative(&self, pos: &X, time: &T) -> X;
 }
 
-pub trait Integrator<X: Dependent<T>, T: Independent> {
+pub trait Integrator<X: Dependent<T>, T: Independent>
+    where Self: Debug
+{
     fn step(&mut self, delta_time: &T) -> X;
     fn dependent(&self) -> X;
     fn independent(&self) -> T;
 }
 
+#[derive(Debug)]
 pub struct Euler<X: Dependent<T>, T: Independent> {
     ode: Box<FirstOrderODE<X, T>>,
     dependent: X,
@@ -44,9 +51,9 @@ impl<X: Dependent<T>, T: Independent> Integrator<X, T> for Euler<X, T> {
         let xdot = self.ode.derivative(&self.dependent, &self.independent);
         self.dependent += xdot * *delta_time;
         self.independent += *delta_time;
-        self.dependent
+        self.dependent.clone()
     }
 
-    fn dependent(&self) -> X { self.dependent }
+    fn dependent(&self) -> X { self.dependent.clone() }
     fn independent(&self) -> T { self.independent }
 }
